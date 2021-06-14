9c1e733bcefc4913937c77d047602a95994af289
Sebastian Tirelli <tirelli@gmail.com>
2021-06-13 21:33:05 -0300

    metamask extension installed validation

--- src/components/SignInPage/SignInPage.tsx
+++ src/components/SignInPage/SignInPage.tsx
@@ -1,14 +1,31 @@
 import { Center, Page, Button } from 'decentraland-ui';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SignInProps } from './SignInPage.types';
+import './SignInPage.css';
+import { useState } from 'react';
 
 const SignInPage = (props: SignInProps) => {
+  const [existMetamask, setExistMetamask] = useState(false);
+
+  useEffect(() => {
+    setExistMetamask(!!(window as any).ethereum);
+  }, []);
+
   return (
     <Page>
       <Center screen>
-        <Button primary onClick={props.onConnect}>
+        <Button primary onClick={props.onConnect} disabled={!existMetamask}>
           Connect
         </Button>
+        {!existMetamask && (
+          <p className="metamask-message">
+            You must install the{' '}
+            <a href="https://metamask.io" target="_blank" rel="noreferrer">
+              Metamask
+            </a>{' '}
+            extension
+          </p>
+        )}
       </Center>
     </Page>
   );
